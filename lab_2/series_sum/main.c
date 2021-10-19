2037983b41f80e48d4c69c37decf6b230bffa572
glassnekeep <56785261+glassnekeep@users.noreply.github.com>
2021-10-19 23:01:19 +0300

    uploaded secoond file

---
+++ lab_2/series_sum/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int n;
+    double X;
+    int previous_factorial = 1;
+    printf("Please enter n\n");
+    scanf_s("%d", &n);
+    printf("Please enter X\n");
+    scanf_s("%lf", &X);
+    double x = X > 0 ? X : X * (-1);
+    double S = 2 + pow(x / 2, n);
+    for(int i = 1; i <= n; i++) {
+        S += i * previous_factorial * 2 + pow(x / 2,  n + 2 * i);
+        previous_factorial *= i;
+    }
+    printf("The sum of this series with these parameters is equal to %f", S);
+    return 0;
+}
